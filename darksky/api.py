c030b36ec46ffd1198a2f5c832b4fd5acce1bcdb
Artyom <detrous@protonmail.com>
2019-12-11 09:36:53 +0200

    Timezone parameter & auto format (#32)
    * Auto format with black&isort & gitignore for vim
    
    * Timezone param

--- darksky/api.py
+++ darksky/api.py
@@ -1,111 +1,163 @@
-import requests
-import aiohttp
 from datetime import datetime
 
-from .forecast import Forecast
+import aiohttp
+import requests
+
 from .exceptions import DarkSkyException
+from .forecast import Forecast
+from .request_manager import (BaseRequestManger, RequestManger,
+                              RequestMangerAsync)
 from .types import languages, units, weather
-from .request_manager import BaseRequestManger, RequestManger, RequestMangerAsync
 
 
 class BaseDarkSky(object):
-    HOST = 'https://api.darksky.net/forecast'
+    HOST = "https://api.darksky.net/forecast"
 
     def __init__(self, api_key: str):
         self.api_key: str = api_key
         self.request_manager: BaseRequestManger = None
 
-    def get_forecast(self, 
-        latitude: float, longitude: float, extend: bool=None, lang=languages.ENGLISH, 
-        units=units.AUTO, exclude: [weather]=None):
+    def get_forecast(
+        self,
+        latitude: float,
+        longitude: float,
+        extend: bool = None,
+        lang=languages.ENGLISH,
+        units=units.AUTO,
+        exclude: [weather] = None,
+    ):
         raise NotImplementedError
 
     def get_time_machine_forecast(
-        self, latitude: float, longitude: float, time: datetime, extend: bool=False, 
-        lang=languages.ENGLISH, units=units.AUTO, exclude: [weather]=None):
+        self,
+        latitude: float,
+        longitude: float,
+        time: datetime,
+        extend: bool = False,
+        lang=languages.ENGLISH,
+        units=units.AUTO,
+        exclude: [weather] = None,
+    ):
         raise NotImplementedError
 
     def get_url(self, latitude: float, longitude: float, time=None, **params):
         if time is None:
-            return '{host}/{api_key}/{latitude},{longitude}'.format(
-                api_key=self.api_key,
-                host=self.HOST,
-                latitude=latitude,
-                longitude=longitude
-            )
-        else: 
-            return '{host}/{api_key}/{latitude},{longitude},{time}'.format(
+            return "{host}/{api_key}/{latitude},{longitude}".format(
                 api_key=self.api_key,
                 host=self.HOST,
                 latitude=latitude,
                 longitude=longitude,
-                time=time
+            )
+        else:
+            return "{host}/{api_key}/{latitude},{longitude},{time}".format(
+                api_key=self.api_key,
+                host=self.HOST,
+                latitude=latitude,
+                longitude=longitude,
+                time=time,
             )
 
 
 class DarkSky(BaseDarkSky):
-
     def __init__(self, api_key: str, gzip: bool = True):
         super().__init__(api_key)
         self.request_manager = RequestManger(gzip)
 
-    def get_forecast(self, 
-        latitude: float, longitude: float, extend: bool=None, lang=languages.ENGLISH, 
-        units=units.AUTO, exclude: [weather]=None):
+    def get_forecast(
+        self,
+        latitude: float,
+        longitude: float,
+        extend: bool = None,
+        lang=languages.ENGLISH,
+        units=units.AUTO,
+        exclude: [weather] = None,
+        timezone: str = None,
+    ):
         url = self.get_url(latitude, longitude)
         data = self.request_manager.make_request(
             url=url,
-            extend=weather.HOURLY if extend else None, 
-            lang=lang, 
+            extend=weather.HOURLY if extend else None,
+            lang=lang,
             units=units,
-            exclude=exclude
+            exclude=exclude,
+            timezone=timezone,
         )
         return Forecast(**data)
 
     def get_time_machine_forecast(
-        self, latitude: float, longitude: float, time: datetime, extend: bool=False, 
-        lang=languages.ENGLISH, units=units.AUTO, exclude: [weather]=None):
+        self,
+        latitude: float,
+        longitude: float,
+        time: datetime,
+        extend: bool = False,
+        lang=languages.ENGLISH,
+        units=units.AUTO,
+        exclude: [weather] = None,
+        timezone: str = None,
+    ):
         url = self.get_url(latitude, longitude, int(time.timestamp()))
         data = self.request_manager.make_request(
             url=url,
-            extend=weather.HOURLY if extend else None, 
-            lang=lang, 
+            extend=weather.HOURLY if extend else None,
+            lang=lang,
             units=units,
-            exclude=exclude
+            exclude=exclude,
+            timezone=timezone,
         )
         return Forecast(**data)
 
 
 class DarkSkyAsync(BaseDarkSky):
-
-    def __init__(self, api_key: str, gzip: bool = True, client_session: aiohttp.ClientSession = None):
+    def __init__(
+        self,
+        api_key: str,
+        gzip: bool = True,
+        client_session: aiohttp.ClientSession = None,
+    ):
         super().__init__(api_key)
-        self.request_manager = RequestMangerAsync(gzip=gzip, client_session=client_session)
+        self.request_manager = RequestMangerAsync(
+            gzip=gzip, client_session=client_session
+        )
 
-    async def get_forecast(self, 
-        latitude: float, longitude: float, extend: bool=None, lang=languages.ENGLISH, 
-        units=units.AUTO, exclude: [weather]=None):
+    async def get_forecast(
+        self,
+        latitude: float,
+        longitude: float,
+        extend: bool = None,
+        lang=languages.ENGLISH,
+        units=units.AUTO,
+        exclude: [weather] = None,
+        timezone: str = None,
+    ):
         url = self.get_url(latitude, longitude)
         data = await self.request_manager.make_request(
             url=url,
-            extend=weather.HOURLY if extend else None, 
-            lang=lang, 
+            extend=weather.HOURLY if extend else None,
+            lang=lang,
             units=units,
-            exclude=exclude
+            exclude=exclude,
+            timezone=timezone,
         )
         return Forecast(**data)
 
     async def get_time_machine_forecast(
-        self, latitude: float, longitude: float, time: datetime, extend: bool=False, 
-        lang=languages.ENGLISH, units=units.AUTO, exclude: [weather]=None):
+        self,
+        latitude: float,
+        longitude: float,
+        time: datetime,
+        extend: bool = False,
+        lang=languages.ENGLISH,
+        units=units.AUTO,
+        exclude: [weather] = None,
+        timezone: str = None,
+    ):
         url = self.get_url(latitude, longitude, int(time.timestamp()))
         data = await self.request_manager.make_request(
             url=url,
-            extend=weather.HOURLY if extend else None, 
-            lang=lang, 
+            extend=weather.HOURLY if extend else None,
+            lang=lang,
             units=units,
-            exclude=exclude
+            exclude=exclude,
+            timezone=timezone,
         )
         return Forecast(**data)
-
-
--- setup.py
+++ setup.py
@@ -1,39 +1,32 @@
 import os
-from setuptools import setup, find_packages
 
-__version__ = '1.5.1'
+from setuptools import find_packages, setup
+
+__version__ = "1.6.0"
 
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
     README = f.read()
 
 
 setup(
     version=__version__,
-    name='darksky_weather',
+    name="darksky_weather",
     packages=find_packages(),
-
-    install_requires=[
-        'requests==2.21.0',
-        'pytz==2019.1',
-        'aiohttp==3.5.4'
-    ],
-
-    description='The Dark Sky API wrapper',
-    long_description='View on github',
-
-    author='Detrous',
-    author_email='detrous@protonmail.com',
-
-    url='https://github.com/Detrous/darksky_weather',
-    download_url='https://github.com/Detrous/darksky_weather/archive/%s.tar.gz' % __version__,
-
-    license='GPLv3 License',
+    install_requires=["requests==2.21.0", "pytz==2019.1", "aiohttp==3.5.4"],
+    description="The Dark Sky API wrapper",
+    long_description="View on github",
+    author="Detrous",
+    author_email="detrous@protonmail.com",
+    url="https://github.com/Detrous/darksky_weather",
+    download_url="https://github.com/Detrous/darksky_weather/archive/%s.tar.gz"
+    % __version__,
+    license="GPLv3 License",
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
